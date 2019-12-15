5e580fa00b35fdd608eef16a3ab84680fe5c626a
kqian5 <kycqian1005@gmail.com>
2019-12-15 16:19:23 -0500

    final

--- model.py
+++ model.py
@@ -10,13 +10,14 @@ class Model(tf.keras.Model):
 
         super(Model, self).__init__()
 
+        # set hyperparameters
         self.vocab_size = vocab_size
         self.embedding_size = 40
         self.window_size = 50
         self.batch_size = 50
-        # self.optimizer = tf.keras.optimizers.RMSprop()
         self.optimizer = tf.keras.optimizers.Adam()
 
+        # establish model and layers
         model = tf.keras.Sequential()
         model.add(Embedding(self.vocab_size, self.embedding_size, input_length=self.window_size))
         model.add(LSTM(256, return_sequences=True))
@@ -29,13 +30,13 @@ class Model(tf.keras.Model):
 
 
     def call(self, inputs):
-        # inputs = tf.convert_to_tensor(inputs)
         return self.model(inputs)
         
     def loss(self, logits, labels):
         return tf.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(labels, logits))
 
 def train(model, train_inputs, train_labels):
+    # train on batches
     for i in range(0, len(train_inputs), model.batch_size * model.window_size):
         with tf.GradientTape() as tape:
             if i + model.batch_size * model.window_size > len(train_inputs):
@@ -44,35 +45,19 @@ def train(model, train_inputs, train_labels):
             labels = np.array(train_labels[i:i+(model.batch_size*model.window_size)]).reshape((model.batch_size, model.window_size))
             logits = model.call(inputs)
             loss = model.loss(logits, labels)
-        print(loss)
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 
-# # Test the model by generating some samples.
-# def test(model):
-
-
 def generate_music(note1, length, vocab, model):
-    """
-    Takes a model, vocab, selects from the most likely next word from the model's distribution
-
-    This is only for your own exploration. What do the sequences your RNN generates look like?
-    
-    :param model: trained RNN model
-    :param vocab: dictionary, word to id mapping
-    :return: None
-    """
-
+    # when given a starting note, predict music for a certain length
     reverse_vocab = {idx:chord for chord, idx in vocab.items()}
 
     first_note = note1
     first_note_index = vocab[note1]
-    # first_word_index = note1
     next_input = np.zeros(shape=(1, 1))
-    next_input[0][0] = first_note
-    # text = [reverse_vocab[note1]]
-    text= [first_note]
+    next_input[0][0] = first_note_index
+    song = [first_note]
 
     for i in range(length):
         p = np.random.uniform()
@@ -82,13 +67,12 @@ def generate_music(note1, length, vocab, model):
         else:
             logits[0][0][0] += 1 - sum(logits[0][0])
             out_index = np.random.choice(list(vocab.values()), p=logits[0][0])
-        text.append(reverse_vocab[out_index])
+        song.append(reverse_vocab[out_index])
         next_input = np.array([np.array([out_index])])
+    return song
 
-    print(text)
-    return text
-
-def convert_to_midi(inputs):
+def convert_to_midi(inputs, f, output_fs):
+    # convert model output back to midi file
     array_piano_roll = np.zeros((128, 1000), dtype=np.int16)
     for t, chord in enumerate(inputs):
         if chord =="unk":
@@ -97,45 +81,24 @@ def convert_to_midi(inputs):
             notes = chord.split(",")
             for n in notes:
                 array_piano_roll[int(n), t] = 1
-    midi = piano_roll_to_pretty_midi(array_piano_roll, fs=3)
+    midi = piano_roll_to_pretty_midi(array_piano_roll, fs=output_fs)
     for note in midi.instruments[0].notes:
         note.velocity = np.random.randint(low=50, high=120)
-    midi.write("results.mid")
+    midi.write(f)
 
 def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0):
-    '''Convert a Piano Roll array into a PrettyMidi object
-     with a single instrument.
-    Parameters
-    ----------
-    piano_roll : np.ndarray, shape=(128,frames), dtype=int
-        Piano roll of one instrument
-    fs : int
-        Sampling frequency of the columns, i.e. each column is spaced apart
-        by ``1./fs`` seconds.
-    program : int
-        The program number of the instrument.
-    Returns
-    -------
-    midi_object : pretty_midi.PrettyMIDI
-        A pretty_midi.PrettyMIDI class instance describing
-        the piano roll.
-    '''
     notes, frames = piano_roll.shape
     pm = pretty_midi.PrettyMIDI()
     instrument = pretty_midi.Instrument(program=program)
 
-    # pad 1 column of zeros so we can acknowledge inital and ending events
     piano_roll = np.pad(piano_roll, [(0, 0), (1, 1)], 'constant')
 
-    # use changes in velocities to find note on / note off events
     velocity_changes = np.nonzero(np.diff(piano_roll).T)
 
-    # keep track on velocities and note on times
     prev_velocities = np.zeros(notes, dtype=int)
     note_on_time = np.zeros(notes)
 
     for time, note in zip(*velocity_changes):
-        # use time + 1 because of padding above
         velocity = piano_roll[note, time + 1]
         time = time / fs
         if velocity > 0:
@@ -156,16 +119,24 @@ def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0):
 
 
 def main():
-    print("started")
+    # preprocess
     data, vocab = process()
 
     train_inputs = data[0:len(data)-1]
     train_labels = data[1:len(data)]
 
+    # train model
     model = Model(len(vocab))
 
     train(model, train_inputs, train_labels)
-    convert_to_midi(generate_music('67', 200, vocab, model))
+    reverse_vocab = {idx:chord for chord, idx in vocab.items()}
+
+    # generate output
+    num_outputs = 10
+    output_fs = 3
+    for i in range(num_outputs):
+        seed = np.random.randint(low=1, high=len(vocab) - 1)
+        convert_to_midi(generate_music(str(reverse_vocab[seed]), 200, vocab, model), "result" + str(i + 1) + ".mid", output_fs)
 
 if __name__ == '__main__':
    main()
--- preprocess.py
+++ preprocess.py
@@ -4,14 +4,16 @@ import numpy as np
 import sys
 import pickle
 
-# only testing for one file rn
 def get_data(f):
+	# get the sequence of notes from a specific file
 	np.set_printoptions(threshold=np.inf)
 	midi_pretty_format = pretty_midi.PrettyMIDI(f)
-	piano_midi = midi_pretty_format.instruments[0] # Get the piano channels
+	# get the piano part
+	piano_midi = midi_pretty_format.instruments[0]
 	piano_roll = piano_midi.get_piano_roll(fs=2)
 	seq = []
 
+	# extract notes
 	for i in range(len(piano_roll[0])):
 		curr_notes = ''
 		for j in range(len(piano_roll)):
@@ -22,10 +24,7 @@ def get_data(f):
 					curr_notes = curr_notes + ',' + str(j)
 		if not curr_notes:
 			curr_notes = 'unk'
-		# if curr_notes == '':
-		# 	continue
 		seq.append(curr_notes)
-	# print(seq[:50])
 	return seq
 
 
@@ -33,44 +32,34 @@ def process():
 	print("preprocessing started")
 	sys.stdout.flush()
 	data = []
+	# preprocess for all the different midi directories
 	for i in range(1, 99):
 		data.extend(get_data('../maestro-v2.0.0/2018/2018midi (' + str(i) + ').midi'))
-	# for i in range(1, 140):
-	# 	data.extend(get_data('../maestro-v2.0.0/2017/2017midi (' + str(i) + ').midi'))
-	# for i in range(1, 129):
-	# 	data.extend(get_data('../maestro-v2.0.0/2015/2015midi (' + str(i) + ').midi'))
-	# for i in range(1, 105):
-	# 	data.extend(get_data('../maestro-v2.0.0/2014/2014midi (' + str(i) + ').midi'))
-	# for i in range(1, 127):
-	# 	data.extend(get_data('../maestro-v2.0.0/2013/2013midi (' + str(i) + ').midi'))
-	# for i in range(1, 163):
-	# 	data.extend(get_data('../maestro-v2.0.0/2011/2011midi (' + str(i) + ').midi'))
-	# for i in range(1, 125):
-	# 	data.extend(get_data('../maestro-v2.0.0/2009/2009midi (' + str(i) + ').midi'))
-	# for i in range(1, 147):
-	# 	data.extend(get_data('../maestro-v2.0.0/2008/2008midi (' + str(i) + ').midi'))
-	# for i in range(1, 115):
-	# 	data.extend(get_data('../maestro-v2.0.0/2006/2006midi (' + str(i) + ').midi'))
-	# for i in range(1, 132):
-	# 	data.extend(get_data('../maestro-v2.0.0/2004/2004midi (' + str(i) + ').midi'))
+	for i in range(1, 140):
+		data.extend(get_data('../maestro-v2.0.0/2017/2017midi (' + str(i) + ').midi'))
+	for i in range(1, 129):
+		data.extend(get_data('../maestro-v2.0.0/2015/2015midi (' + str(i) + ').midi'))
+	for i in range(1, 105):
+		data.extend(get_data('../maestro-v2.0.0/2014/2014midi (' + str(i) + ').midi'))
+	for i in range(1, 127):
+		data.extend(get_data('../maestro-v2.0.0/2013/2013midi (' + str(i) + ').midi'))
+	for i in range(1, 163):
+		data.extend(get_data('../maestro-v2.0.0/2011/2011midi (' + str(i) + ').midi'))
+	for i in range(1, 125):
+		data.extend(get_data('../maestro-v2.0.0/2009/2009midi (' + str(i) + ').midi'))
+	for i in range(1, 147):
+		data.extend(get_data('../maestro-v2.0.0/2008/2008midi (' + str(i) + ').midi'))
+	for i in range(1, 115):
+		data.extend(get_data('../maestro-v2.0.0/2006/2006midi (' + str(i) + ').midi'))
+	for i in range(1, 132):
+		data.extend(get_data('../maestro-v2.0.0/2004/2004midi (' + str(i) + ').midi'))
 
-	# with open('data.pkl', 'wb') as f:
-	# 	pickle.dump(data, f)
-	# with open('data.pkl', 'rb') as f:
-	# 	data = pickle.load(f)
+	# construct tokenized data and vocabulary
 	vocab = {n:i for i, n in enumerate(list(set(data)))}
 	tokenized = []
 	for chord in data:
 		tokenized.append(vocab[chord])
 	print("preprocessing complete")
-	# with open('tokenized.pkl', 'rb') as f1:
-	# 	tokenized = pickle.load(f1)
-	# f1.close()
-	# with open('vocab.pkl', 'rb') as f2:
-	# 	vocab = pickle.load(f2)
-	# f2.close()
 	return tokenized, vocab
-
-# print(get_data('../maestro-v2.0.0/2018/2018midi (1).midi'))
 
 
